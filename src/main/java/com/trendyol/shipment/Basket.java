83c786089df70bc482c75076d6ba7896c5f1fe39
pelinceliksoz <celikspzpelin@gmail.com>
2023-09-10 20:26:00 +0300

    fill getShipmentSize() in Basket.java

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,39 @@
 package com.trendyol.shipment;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+
+        if (products == null || products.isEmpty()) {
+            return null;
+        }
+
+        Map<ShipmentSize, Integer> sizeCountMap = new HashMap<>();
+        for (Product product : products) {
+            ShipmentSize size = product.getSize();
+            sizeCountMap.put(size, sizeCountMap.getOrDefault(size, 0) + 1);
+        }
+
+        for (Map.Entry<ShipmentSize, Integer> entry : sizeCountMap.entrySet()) {
+            if (entry.getValue() >= 3) {
+                return getNextLargerSize(entry.getKey());
+            }
+        }
+
+        ShipmentSize largestSize = Collections.max(sizeCountMap.keySet());
+
+        if (largestSize == ShipmentSize.X_LARGE) {
+            return ShipmentSize.X_LARGE;
+        }
+
+        return largestSize;
     }
 
     public List<Product> getProducts() {
@@ -16,5 +42,16 @@ public class Basket {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    private ShipmentSize getNextLargerSize(ShipmentSize size) {
+        switch (size) {
+            case SMALL:
+                return ShipmentSize.MEDIUM;
+            case MEDIUM:
+                return ShipmentSize.LARGE;
+            default:
+                return ShipmentSize.X_LARGE;
+        }
     }
 }
